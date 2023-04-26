53c1b766dcbc43a9fb99b791aa452b187e88c558
Liza <lizavolykova@mail.ru>
2023-04-26 21:32:29 +0300

    eeeeee

--- womo/registration/admin.py
+++ womo/registration/admin.py
@@ -1,15 +1,1 @@
 from django.contrib import admin
-from .models import User, ToDoList
-
-class ToDoListInline(admin.TabularInline):
-    model = ToDoList
-
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    inlines = [ToDoListInline]
-
-
-@admin.register(ToDoList)
-class ToDoListAdmin(admin.ModelAdmin):
-    list_display = ('user_id', 'delo', 'importance')
