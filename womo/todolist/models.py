41611071f875baea815f6dcc12b2bc2fb72d0d3c
AliceSkoropisova <alicescoropisova@gmail.com>
2023-04-28 12:39:53 +0300

    bombi

--- womo/todolist/models.py
+++ womo/todolist/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from registration.models import User
 
 class ToDoList(models.Model):
-    user = models.ForeignKey(User,
-    default = '0',
+    user_id = models.ForeignKey(User,
     on_delete = models.CASCADE,
     help_text = "Введите id",
     verbose_name = "ID")
@@ -16,6 +15,6 @@ class ToDoList(models.Model):
     Choices_Importance = [(Imp, "Важно"), (Mid, "Средне"), (Not_imp, "Не важно")]
     importance = models.CharField(max_length = 8, choices = Choices_Importance, default = Imp)
 class Meta:
-    ordering = ["user", "importance"]
+    ordering = ["user_id", "importance"]
 def __str__(self):
     return self.delo
