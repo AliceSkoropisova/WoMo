0e7245b0297edb92298d66d2be4aee88ddea37ee
Liza <lizavolykova@mail.ru>
2023-04-28 11:39:19 +0300

    sjdfbndlobfjnhdlf

--- womo/registration/models.py
+++ womo/registration/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+
+class User(models.Model):
+    user_id = models.CharField(max_length = 10,
+                               help_text = "Введите айди",
+                               verbose_name = "ID")
+    def __str__(self):
+        return self.user_id
--- womo/womo/urls.py
+++ womo/womo/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path
 #from registration import views
 from django.urls import path, include
+from todolist import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
     #path('registration/', views.register, name='register')
 ]
